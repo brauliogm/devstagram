6ac08f81f17d5a03e52ae33787ff084671f0ae9d
brauliogm <brauliogm16@gmail.com>
2024-08-28 20:27:45 -0600

    first commit

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Nosotros papu
+@endsection
+
+@section('contenido')
+
+    <h2>
+        Contenido de nosotros
+
+    </h2>
+@endsection
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Pagina Principal papu
+@endsection
+
+@section('contenido')
+
+    <h2>
+        Contenido papu
+
+    </h2>
+@endsection
